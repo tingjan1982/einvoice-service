69ba166376f0b35543008a80b71355d2d7dbadb0
joelin <tingjan1982@gmail.com>
2020-12-21 17:46:48 +0800

    Add CORS support

--- src/main/java/io/nextpos/einvoice/einvoicemessage/web/EInvoiceMessageController.java
+++ src/main/java/io/nextpos/einvoice/einvoicemessage/web/EInvoiceMessageController.java
@@ -19,6 +19,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/einvoice")
+@CrossOrigin
 public class EInvoiceMessageController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(EInvoiceMessageController.class);
